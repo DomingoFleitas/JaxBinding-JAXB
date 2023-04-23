7e68d05b88f46ecf9f23509b8a41c06859134a1c
DomingoFleitas <>
2023-04-23 04:51:14 +0100

    Read and Write XML with Java Maven JAXB. Lectura y Escritura de XML con Java Maven JAXB.

---
+++ README.md
@@ -0,0 +1,7 @@
+# JaxBinding-JAXB
+
+Read and Write XML with Java Maven JAXB [Java Architecture for XML Binding]. 
+
+Lectura y Escritura de XML con Java Maven JAXB [Java Architecture for XML Binding].
+
+----
---
+++ src/main/java/jaxbinding/JaxBinding.java
@@ -0,0 +1,10 @@
+package jaxbinding;
+
+public class JaxBinding {
+
+    public static void main(String[] args) {
+        JConcesionario con = new JConcesionario();
+        con.setVisible(true);
+        con.setLocationRelativeTo(null);
+    }
+}
